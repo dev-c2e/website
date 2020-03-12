06b4845e6cb41bf3519cdaaca9988df019f9d82d
Andriamaherisoa <andriamaherisoamahandrisoa@gmail.com>
2020-03-12 20:04:06 +0100

    feat: adding devDependencies and configure set tests up

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,3 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App component test', () => {
+  const wrapper = shallow(<App />);
+  test('defined value assignation', () => {
+    expect(wrapper).toBeDefined();
+  });
+});
