781eba1aeeeef9034cccd12fa82709d819d69ab9
Judionit <judiandriamahandry@gmail.com>
2020-03-14 21:35:24 +0200

    adding a simple helmet, improve in future

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home page</title>
+        <meta name="description" content="club d'entraide de l'ENI" />
+      </Helmet>
       <h2 className="App-logo">C2E</h2>
     </div>
   );
